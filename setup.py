9e32b9b452930c20f3cb2fc196491e6256cc7a60
Stefan Kjartansson <esteban.supreme@gmail.com>
2012-06-10 15:49:16 +0000

    updated setup file

--- setup.py
+++ setup.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+"""
 
+try:
+    import multiprocessing
+except ImportError:
+    pass
 
 try:
     from setuptools import setup, find_packages
 except ImportError:
-    from distutils.core import setup, find_packages
+    from distutils.core import setup, find_packages  # noqa
 
+install_requires = [
+    'requests>=0.13.1',
+]
+
+tests_require = [
+    'nose>=1.1.2',
+]
 
 setup(
-    name = "korta",
-    version = "0.1.3",
-    packages = find_packages(),
-    install_requires = [
-        'pycurl',
-    ],
-    package_data = {
-        'korta': ['certs/*.pem', 'certs/*.crt'],
+    name="korta",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=install_requires,
+    package_data={
+        'korta': ['certs/*.pem'],
     },
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    test_suite='nose.collector',
     author="Stefan Kjartansson",
     author_email="esteban.supreme@gmail.com",
     description="Korta Client library",
